f79ef3bd79065e0adbefbf71f84fd86b73be7850
David Petrofsky <davidpet@ucla.edu>
2025-07-31 01:26:46 +0000

    Finish authentication"

--- blango/settings.py
+++ blango/settings.py
@@ -47,6 +47,7 @@ class Dev(Configuration):
         'crispy_forms',
         'crispy_bootstrap5',
         'rest_framework',
+        'rest_framework.authtoken',
         'debug_toolbar',
         'blango_auth',
         'blog',
@@ -212,6 +213,14 @@ class Dev(Configuration):
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
 class Prod(Dev):
   DEBUG = False
