1793461efa6f9d570c58b538d257719993c111ad
David Petrofsky <davidpet@ucla.edu>
2025-08-05 19:54:23 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os 
+from datetime import timedelta
 
 from pathlib import Path
 from configurations import Configuration
@@ -228,6 +229,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -250,6 +252,11 @@ class Dev(Configuration):
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter",
         ],
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
 class Prod(Dev):
